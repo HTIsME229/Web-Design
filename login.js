10a6dec4087b540107c22d157520801abe745869
thangle229 <leviethoangthang1234@gmail.com>
2024-04-06 21:35:24 +0700

    Mô Phật fix BUg

--- login.js
+++ login.js
@@ -1,6 +1,13 @@
 // let box=document.querySelector(".box_login")
 let exitLogin = document.querySelector(".box_login .button");
-let exitSignUp=document.querySelector(".box_signUp .button")
+let exitSignUp=document.querySelector(".box_signUp .button");
+let button_login=document.querySelector(".button-login");
+let box_login=document.querySelector(".box_login");
+let button_Signup=document.querySelector(".button-signUp");
+let box_button=document.querySelector(".box-button");
+let box_signup=document.querySelector(".box_signUp");
+let signUp=document.querySelector(".signup");   
+
 exitLogin.onclick = function(){
 
        box_login.style.display="none"
@@ -24,16 +31,30 @@ document.onkeydown = function (e) {
 break;
     }
 };
-let button_login=document.querySelector(".button-login")
-let box_login=document.querySelector(".box_login")
-let button_Signup=document.querySelector(".button-signUp")
-let box_button=document.querySelector(".box-button")
-let box_signup=document.querySelector(".box_signUp")
-let signUp=document.querySelector(".signup")
-let mail_reg=document.querySelector(".box_signUp .email .content-input")
-let pass_reg=document.querySelector(".box_signUp .password .content-input")
-let check_box=document.querySelector(".check")
 
+
+
+
+signUp.addEventListener('click',function () {
+    event.preventDefault()
+    let check_box=document.querySelector(".check").checked; 
+
+    let pass_reg=document.querySelector(".box_signUp .password .content-input").value;
+    
+    let mail_reg=document.querySelector(".box_signUp .email .content-input").value;
+    if(mail_reg!=""&& pass_reg!=""&& check_box)
+ {     
+      box_login.style.display="block";
+ box_button.style.display="none";
+    box_signup.style.display="none";
+}
+else if(mail_reg=="")
+  alert(`Nhap Mail`)
+else if(pass_reg=="")
+alert(`Nhap Password`)
+else
+alert(`Xác nhận điều khoản`)
+})
 button_login.onclick=function(){
     box_button.style.display="none"
     box_login.style.display="initial"
@@ -42,22 +63,4 @@ button_Signup.onclick=function(){
  
     box_button.style.display="none"
     box_signup.style.display="initial"
-}
-
-signUp.onclick=function(){
-    if(mail_reg.value!=""&&pass_reg.value!=""&&check_box.value)
-    {
-    box_button.style.display="none"
-
-    box_signup.style.display="none"
-
-    box_login.style.display="initial"}
-  else if (mail_reg.value=="")
-    alert(`Vui lòng Nhập Gmail`)
-else if(mail_reg.value=="")
-alert(`Vui lòng nhập password`)
-else if(!check_box.value)
-alert("Đồng ý điều khoản sử dụng")
-
-
 }
